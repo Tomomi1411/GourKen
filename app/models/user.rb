368065a4c63f6a5e919db2f20a1a9fd28fae479a
Tomomi1411 <70436578+Tomomi1411@users.noreply.github.com>
2020-10-22 14:45:13 +0900

    gem rails_12factorの追加

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  attachment :profile_image
+  has_many :gourmets, dependent: :destroy
+
+  validates :username, presence: true
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '34d43a781a9efd125f40ffe2100808322bd9e9d9f0db2e3f57066a2bf9e05ad68e44aad70ea31c25db8e235e4884b10d475f0cf25ebdef9aed0fc24a62bb325e'
